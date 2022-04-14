554f397428fbfc1220f901c372bdd8add97e216a
Joaquin Pedrozo <joaquin.pedrozo@gmail.com>
2022-04-13 22:31:32 -0300

    login_ui_mejorado

--- src/components/commons/SeccionLogin.jsx
+++ src/components/commons/SeccionLogin.jsx
@@ -4,15 +4,21 @@ import { api } from "../../api/api";
 export function SeccionLogin(){
     return (<div className="login">        
             <div className="login caja">                
-                <div className="cont">
-                    <h2 className="titulo">LOGIN</h2>
-                     <h3>Usuario:</h3> <input name="usuario" type="text"></input>
-                     <h3>Password:</h3> <input name="clave" type="text"></input>                      
-                </div>
-                <div className="cont">
-                     <button type="submit" className="btnlogin"> Login</button>
-                     <span className="text-danger d-block"></span>                     
-                </div>                                        
+                <form>
+                    <h2 className="titulo">LOGIN</h2>                   
+                    <div className="cont">
+                        <label htmlFor="login-usuario">Usuario:</label>
+                        <input name="usuario" type="text"></input>
+                    </div>
+                    <div className="cont">
+                        <label htmlFor="login-password">Password:</label>
+                        <input name="clave" type="text"></input>                      
+                    </div>
+                    <div className="cont2">
+                        <button type="submit" className="btnlogin"> Login</button>
+                        <span className="text-danger d-block"></span>                     
+                    </div>
+                </form>                                        
             </div> 
     </div>);
 }
