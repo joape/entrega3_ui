c26d52dfcb7e52dd9663e3e61f58a3d5b34ca11f
Joaquin Pedrozo <joaquin.pedrozo@gmail.com>
2022-04-13 22:04:34 -0300

    login_ui_terminado

--- src/components/commons/SeccionLogin.jsx
+++ src/components/commons/SeccionLogin.jsx
@@ -1,11 +1,18 @@
-//import { useEffect, useState } from "react";
+import { useState } from "react";
+import { api } from "../../api/api";
 
 export function SeccionLogin(){
-    return (<div className="articulos">        
-            <article className="detalle">
-                LOGIN
-                Usuario:
-                Password
-            </article> 
+    return (<div className="login">        
+            <div className="login caja">                
+                <div className="cont">
+                    <h2 className="titulo">LOGIN</h2>
+                     <h3>Usuario:</h3> <input name="usuario" type="text"></input>
+                     <h3>Password:</h3> <input name="clave" type="text"></input>                      
+                </div>
+                <div className="cont">
+                     <button type="submit" className="btnlogin"> Login</button>
+                     <span className="text-danger d-block"></span>                     
+                </div>                                        
+            </div> 
     </div>);
 }
