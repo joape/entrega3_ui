41d62bf4af6acf7fef00ab0a4e8474eaf3031af1
Joaquin Pedrozo <joaquin.pedrozo@gmail.com>
2022-04-17 19:59:07 -0300

    Mejoras_en_Login

--- src/components/commons/SeccionLogin.jsx
+++ src/components/commons/SeccionLogin.jsx
@@ -2,29 +2,42 @@ import { useState } from "react";
 import { api } from "../../api/api";
 
 export function SeccionLogin(){
-    /*defino las variables para los estados*/
+    //defino las variables para los estados - Nos permite redibujar en el formulario
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false); //uso este estado para cambiar boton.
     const [error, setError] = useState("");
 
     //El formulario se manda
     const handleSubmit = (event) => {
-    event.preventDefault(); /* detengo el comportamiento por default*/
-
-        //Cambiar el estado loading a true
+        event.preventDefault(); //detengo el comportamiento por default
+        //console.log(email);
+        //console.log(password);
+        
+        //Cambiar el estado loading a true para que se dibuje y muestre "Validando"
         setLoading(true);
 
         //Borramos el error
         setError("");
 
-        //Llamar un POST con axios a /login y mandar la info en el
-        //estado actual
-        api.get("/usuarios", { email: email, password: password }).then(
-        (response) => {
-            console.log(response);
+        //Borro o Inicializo los estados de los inputs
+        //setEmail("");
+        //setPassword("");
 
-            //Cambiar el estado loading a false
+        //armo lo que voy a mandar en el POST.
+        //Si tengo que mandar archivos debo instanciar un FormData previo al envio POST
+        //Si necesitara enviar un archivo, deberia usar el formData.append("correo", email)
+        //con cada uno de los valores a enviar, suplantando el const data
+        const data = { //esto es un objeto
+            email: email, 
+            password: password 
+        };
+
+        //Llamar un POST con axios a /login y mandar la info en el estado actual. 
+        api.post("/usuarios", data).then((response) => {
+                console.log(response);
+
+            //Cambiar el estado de loading a false cuando me responde OK la API
             setLoading(false);
         },
         (errorResponse) => {
@@ -36,10 +49,18 @@ export function SeccionLogin(){
 
             //Cambiar el estado loading a false
             setLoading(false);
-        }
-        );
-    
+        });    
     };
+
+    //Maneja estados del Formulario
+    const handleEmailChange = (event) => { //aca manejo que se redibuje el email (componente controlado)
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => { //aca manejo que se redibuje la clave (componente controlado)
+        setPassword(event.target.value); //target hace referencia al input que origino el evento. Por eso pedimos el value
+    };
+
     return (<div className="login">        
             <div className="login caja">                
                 <form onSubmit={handleSubmit}>
@@ -47,6 +68,8 @@ export function SeccionLogin(){
                     <div className="cont">
                         <label htmlFor="login-usuario">Usuario:</label>
                         <input 
+                            value={email}
+                            onChange={handleEmailChange}
                             name="usuario" 
                             type="email"
                             id="login-usuario"
@@ -56,7 +79,9 @@ export function SeccionLogin(){
                     </div>
                     <div className="cont">
                         <label htmlFor="login-password">Password:</label>
-                        <input 
+                        <input
+                            value={password}
+                            onChange={handlePasswordChange}
                             name="clave" 
                             type="password"
                             id="login-password"
@@ -65,7 +90,9 @@ export function SeccionLogin(){
                         />                      
                     </div>
                     <div className="cont2">
-                        <button type="submit" className="btnlogin"> Login</button>
+                        <button type="submit" className="btnlogin">
+                            {loading ? "Validando" : "Login"} 
+                        </button>
                         <span className="text-danger d-block"></span>                     
                     </div>
                 </form>                                        
